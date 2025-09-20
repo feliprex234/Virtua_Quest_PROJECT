9aa2d3c88a01a4e9c10b9f516a08a8c118e36e65
feliprex234 <feliprex234@gmail.com>
2025-09-19 21:19:28 -0300

    VDev0.2
    Atualizado:
    
    - Sprite
    - TileMap
    - Colisões
    - Controles

--- virtua-quest/Scripts/player.gd
+++ virtua-quest/Scripts/player.gd
@@ -1,16 +1,37 @@
 extends CharacterBody2D
 
-var move_speed = 480
-var gravity = 1200
-var jump_force = -720
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+
+const SPEED = 80.0
+const JUMP_VELOCITY = -300.0
+
 
 func _physics_process(delta: float) -> void:
-	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	
-	if Input.is_action_pressed("jump"):
-		velocity.y = jump_force / 2
-	velocity.y += gravity * delta
-	
-	velocity.x = move_speed * move_direction
-	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if is_on_floor():
+		if direction > 0:
+			anim.flip_h = false
+			anim.play("walk")
+		elif direction < 0:
+			anim.flip_h = true
+			anim.play("walk")
+		else:
+			anim.play("idle")
+	else:
+		anim.play("jump")
 	move_and_slide()
